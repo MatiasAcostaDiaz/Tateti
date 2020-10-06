9ec891161ff95bbc275eb1cc24074da0f5e909a9
facu2279 <2279@holbertonschool.com>
2020-10-06 21:07:58 +0000

    a

--- funciones.h
+++ funciones.h
@@ -2,4 +2,4 @@ void print_tablero(char (*tablero)[3]);
 int verificar(char c,char (*tab)[3],int x, int y);
 int generar_random(void);
 int validatec (char (*tablero)[3], int x, int y);
-int validaten ( char c);
+int validaten (int n);
